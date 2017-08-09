d67cbfaf2a20106e945cbb6d9c082f8e54cfda32
hankeet <kyleyue@outlook.com>
2017-08-09 10:01:00 +0800

    添加项目文件。

---
+++ app/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace app.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpPost, ActionName("Login")]
+        public void Login(FormCollection collection)
+        {
+            object obj = SqlHelper.ExecuteScalar("select UserId from CDBUsers where UserName=@uname and Password=@pwd",
+                         new SqlParameter("@uname", collection[0]),
+                         new SqlParameter("@pwd", Weibo.Models.Myencrypt.myencrypt(collection[1])));
+
+
+            if (obj != null)
+            {
+                FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(
+                1,
+                collection[0],
+                DateTime.Now,
+                DateTime.Now.AddMinutes(30),
+                false,
+                "admins"
+                );
+                string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+                System.Web.HttpCookie authCookie = new System.Web.HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+                System.Web.HttpContext.Current.Response.Cookies.Add(authCookie);
+            }
+
+
+            Response.Redirect("~/");
+        }
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ app/Datas/CDBTemplate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace app.Datas
+{
+    public class CDBTemplate
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public class LogOnModel
+        {
+            [Required]
+            [Display(Name ="用户名")]
+            public string UserName { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name ="密码")]
+            public string Password { get; set; }
+
+            [Display(Name ="下次自动登录")]
+            public bool RememberMe { get; set; }
+
+        }
+    }
+}
